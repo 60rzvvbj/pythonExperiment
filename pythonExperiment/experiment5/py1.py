95096ef18a6b4911218ef7468ba7e3f3039eaef2
60rzvvbj <1808078515@qq.com>
2021-04-26 19:15:00 +0800

    update py1.py

--- pythonExperiment/experiment5/py1.py
+++ pythonExperiment/experiment5/py1.py
@@ -1,6 +1,7 @@
+num = 12345
 f = open('resources/test.txt', 'w')
-f.write(str(12345))
+f.write(str(num))
 f.close()
-f = open('resources/test.dat', 'w')
-f.write(str(12345))
+f = open('resources/test.exe', 'wb')
+f.write(str(num).encode())
 f.close()
